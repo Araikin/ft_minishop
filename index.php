72cb1872b4c8a64c6234a6723de99f33b16bb6a6
Aray Sultanbekova <asultanb@e1z3r3p22.42.us.org>
2020-02-29 22:50:47 -0800

    doing login/logout

--- index.php
+++ index.php
@@ -9,25 +9,28 @@ mysqli_select_db($conn, $GLOBALS['db_username']);
 <html>
 	<head>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-		<link rel="stylesheet" type="text/css" href="style.css">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
 	</head>
 	<body>
 		<div class="header">
-			<div>
-				<img src="src/logo.png" width="10%" />
-				<h1>montessori</h1>
-				<i>Bazaar</i>
+			<div id="logo">
+				<img id="logo1" src="src/logo.png" width="10%" />
+				<img id="logo2" src="src/logo2.png" width="20%" />
+                
 			</div>
 			<div class="account">
-				<div><img src="src/cart.png" />Cart</div>
-				<div><img src="src/account.png" />Account</div>
+				<!-- <i class="material-icons cart">local_grocery_store</i> -->
+				<a href="php/register.php"><i id="register" class="material-icons">account_circle</i></a>
+				<p id="register">Sign in/Register</p>
 			</div>
 		</div>
 		<div class="category">
 			<?php echo str_repeat("<a>categ</a>", 5); ?>
 		</div>
 		<div class="flex-container">
-			<?php echo str_repeat("<div></div>", 15); ?>
+			<?php echo str_repeat("<div></div>", 12); ?>
 		</div>
 	</body>
 </html> 
