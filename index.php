406a94bd1bb7cf4536202af1d0e16e3ba47969ac
Matvei Ivanov <matvivan@e1z3r3p18.42.us.org>
2020-03-01 15:43:22 -0800

    almost done

--- index.php
+++ index.php
@@ -4,6 +4,11 @@ include ("php/db.php");
 $conn = connect_db();
 $GLOBALS['conn'] = $conn;
 mysqli_select_db($conn, $GLOBALS['db_username']);
+$CATEG = array("newborn" => array("Toys for young infants—birth through 6 months", "Babies like to look at people—following them with their eyes. Typically, they prefer faces and bright colors. Babies can reach, be fascinated with what their hands and feet can do, lift their heads, turn their heads toward sounds, put things in their mouths, and much more!"),
+	"infant" => array("Toys for older infants—7 to 12 months", "Older babies are movers—typically they go from rolling over and sitting, to scooting, bouncing, creeping, pulling themselves up, and standing. They understand their own names and other common words, can identify body parts, find hidden objects, and put things in and out of containers."),
+	"1 year" => array("Toys for 1-year-olds", "One-year-olds are on the go! Typically they can walk steadily and even climb stairs. They enjoy stories, say their first words, and can play next to other children (but not yet with!). They like to experiment—but need adults to keep them safe."),
+	"toddler" => array("Toys for 2-year-olds (toddlers)", "Toddlers are rapidly learning language and have some sense of danger. Nevertheless they do a lot of physical “testing”: jumping from heights, climbing, hanging by their arms, rolling, and rough-and-tumble play. They have good control of their hands and fingers and like to do things with small objects."),
+	"preschool/kids" => array("Toys for 3- to 6-year-olds (preschoolers and kindergarteners)", "Preschoolers and kindergartners have longer attention spans than toddlers. Typically they talk a lot and ask a lot of questions. They like to experiment with things and with  their still-emerging physical skills. They like to play with friends—and don’t like to lose! They can take turns—and sharing one toy by two or more children is often possible  for older preschoolers and kindergarteners."));
 ?>
 
 <html>
@@ -41,11 +46,14 @@ mysqli_select_db($conn, $GLOBALS['db_username']);
 			</div>
 		</div>
 		<div class="category">
-			<?php echo str_repeat("<a>categoria</a>", 5); ?>
+			<?php
+				foreach ($CATEG as $NAME => $VAL)
+					echo "<a style=color:(#15325C)*($_GET['categ']===$NAME) href=\'?categ=$NAME'>$NAME</a>";
+			?>
 		</div>
 		<div class="title">
-			<h1>Categoria of items</h1>
-			<p>Discription of categoria<br />Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+			<h1><?php echo $CATEG[$_GET["categ"]][0]; ?></h1>
+			<p><?php echo $CATEG[$_GET["categ"]][1]; ?></p>
 		</div>
 		<div class="flex-container">
 			<?php
